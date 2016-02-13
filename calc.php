620e2fe214577d4906cb9b965165e3632af24d75
nakanoyoshiki <n.yoshiki67@gmail.com>
2016-02-14 00:11:25 +0900

    fix

--- calc.php
+++ calc.php
@@ -1,6 +1,12 @@
 <?php
   //値を取得
-
+$left =null;
+$right=null;
+$ope=null;
+$error_conut =0;
+$answer=null;
+$errors = array();
+if($_SERVER['REQUEST_METHOD']== 'POST'){
   if(isset($_POST['leftbox'])){
     $left = $_POST['leftbox'];
   }
@@ -8,10 +14,9 @@
     $right = $_POST['rightbox'];
   }
   if(($_POST['selOpe']) == '+' || '-' || '×' || '÷'){
-    $ope =($_POST['selOpe']);
+    $ope =$_POST['selOpe'];
   }
-  $error_conut =0;
-  //セレクトボックスによって処理を変える
+
   if((is_numeric($left)) && (is_numeric($right))){
     switch ($ope) {
       case "＋":
@@ -26,55 +31,62 @@
         $answer = $left * $right;
         break;
 
-      case "÷": if($right == '0'){
-        print 'このような計算はできません';
+      case "÷":
+        if($right == '0'){
+          $errors['no_division']= 'このような割り算はできません';
+          $error_conut = 1;
+        }else{
+          $answer = $left / $right;
+        }
+        break;
+      default:
         $error_conut = 1;
-      }else{
-        $answer = $left / $right;
-      }break;
-    default:
-      $error_conut = 1;
-      print '四則演算をしてください';
-      break;
+        $errors['no_four_arithmetic'] = '四則演算してください';
+        break;
     }
   }else{
     $error_conut = 1;
-    print 'このような計算はできません';
+    $errors['no_calculation'] ='このような計算はできません';
   }
-
+}
 ?>
 <html>
   <head>
     <meta charset="utf-8">
     <title>電卓</title>
-    <style type="text/css">
-    .p{
-      font-size: 18px;
-    }
-    </style>
+    <style type="text/css"></style>
   </head>
   <body>
+
     <form name="form1" action="" method="post">
-      <input type = "text" name = "leftbox">　
+      <input type = "text" name = "leftbox" value="<?PHP print htmlspecialchars($left);?>">　
       <select name="selOpe" size=1>
         <option value = "＋">＋</option>
         <option value = "－">－</option>
         <option value = "×">×</option>
         <option value = "÷">÷</option>
       </select>　
-
-      <input type = "text" name = "rightbox">
+      <input type = "text" name = "rightbox" value="<?PHP print htmlspecialchars($right);?>">
       <br>
-
       <input type = "submit" value = "計算">
       <input type = "reset" value = "クリア">
     </form>
     <?php
-      if($error_conut == 0){
-        print htmlspecialchars($left." ".$ope." ".$right."   =".$answer);
+      if($_SERVER['REQUEST_METHOD']== 'POST'){
+        if($error_conut == 0){
+          print htmlspecialchars($left . " " . $ope . " " . $right."   = " . $answer);
+        }
       }
     ?>
+    <?php if (count($errors) > 0): ?>
+      <ul>
+        <?php foreach ($errors as $error): ?>
+          <li>
+            <?php echo htmlentities($error , ENT_QUOTES,'UTF-8'); ?>
+          </li>
+        <?php  endforeach; ?>
+      </ul>
+    <?php endif ?>
     <br>
-    <a href="#" onclick="history.back(); return false;">前の画面へ戻る</a>
   </body>
 </html>
